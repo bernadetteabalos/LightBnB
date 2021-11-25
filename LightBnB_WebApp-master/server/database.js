79487a3facaea48b2eb56272b4b809390bb84c06
Bernadette Abalos <bern.roseabalos@gmail.com>
2021-11-25 18:46:50 +0000

    update getAllReservations to use sql queries.

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -78,7 +78,18 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+    .query(`SELECT reservations.*, properties.*, AVG(property_reviews.rating) AS average_rating
+      FROM reservations
+      JOIN properties ON properties.id = reservations.property_id
+      JOIN property_reviews ON properties.id = reservations.property_id
+      WHERE reservations.guest_id = $1
+      GROUP BY reservations.id, properties.id
+      LIMIT $2`, [guest_id, limit])
+    .then((result) => result.rows)
+    .catch((err) => {
+      console.log(err.message);
+    });
 }
 exports.getAllReservations = getAllReservations;
 
