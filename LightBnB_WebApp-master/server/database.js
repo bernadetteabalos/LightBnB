aa61ca1d52ceb268614f48e249e011264be04d9a
Bernadette Abalos <bern.roseabalos@gmail.com>
2021-11-25 18:36:58 +0000

    Implement functions to use SQL queries on database.

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -18,17 +18,18 @@ const pool = new Pool({
  * @param {String} email The email of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
+ const getUserWithEmail = function(email) {
+
+  return pool
+  .query(`SELECT * FROM users WHERE email = $1`, [email])
+  .then(result => {
+    if (result.rows[0]) {
+      return result.rows[0];
     } else {
-      user = null;
+      return null;      
     }
-  }
-  return Promise.resolve(user);
+  })
+  .catch(error => console.log(error.message));
 }
 exports.getUserWithEmail = getUserWithEmail;
 
@@ -38,7 +39,16 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+  return pool
+  .query(`SELECT * FROM users WHERE id = $1`, [id])
+  .then(result => {
+    if (result.rows[0]) {
+      return result.rows[0];
+    } else {
+      return null;      
+    }
+  })
+  .catch(error => console.log(error.message));
 }
 exports.getUserWithId = getUserWithId;
 
@@ -49,10 +59,14 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  return pool
+    .query(`INSERT INTO users (name, email, password) 
+      VALUES ($1, $2, $3)
+      RETURNING *`, [user.name, user.email, user.password])
+    .then(result => {
+      return result.rows;
+    })
+    .catch(error => console.log(error.message));
 }
 exports.addUser = addUser;
 
